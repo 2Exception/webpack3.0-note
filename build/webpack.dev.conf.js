228ff3144d25dd055a62356f2da01e64feb74e1f
dingjiamughal <465560882@qq.com>
2018-04-04 00:43:19 +0800

    complate middleware-server

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,11 +2,13 @@
  * @Author: Dingjia
  * @Date:   2018-03-31T14:55:48+08:00
  * @Last modified by:   Dingjia
- * @Last modified time: 2018-04-02T00:50:04+08:00
+ * @Last modified time: 2018-04-04T00:20:09+08:00
  */
 
 
 const webpack = require("webpack")
+const proxy = require("./proxy")
+const historyApiFallback = require("./historyfallback")
 module.exports = {
   devtool:"cheap-module-source-map",
   devServer:{
@@ -15,27 +17,8 @@ module.exports = {
     overlay:true,
     hot:true,
     hotOnly:true,
-    // proxy:{
-    //   "/":{
-    //     target:"https://m.weibo.cn",
-    //     changeOrigin:true,
-    //     logLevel:"debug",
-    //     pathRewrite:{
-    //       "^/comments":"/api/comments"
-    //     },
-    //     headers:{
-    //       "Cookie":""
-    //     }
-    //   }
-    // },
-    historyApiFallback:{//html重定向
-      rewrite:{
-        from:/^\/([a-zA-Z0-9]+\/?)([a-zA-Z0-9]+)/,
-        to:function(ctx){
-          return "/" + ctx.match[1] + ctx.match[2] + ".html"
-        }
-      }
-    }
+    proxy:proxy,
+    historyApiFallback:historyApiFallback
   },
   plugins:[
     new webpack.HotModuleReplacementPlugin(),
