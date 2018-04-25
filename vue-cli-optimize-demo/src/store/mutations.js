216739714f637839b3a49fa733b3010aa505446e
dj20150727 <2277904201@qqcpm>
2018-04-25 16:29:38 +0800

    move docs

--- vue-cli-optimize-demo/src/store/mutations.js
+++ vue-cli-optimize-demo/src/store/mutations.js
@@ -2,17 +2,17 @@ import * as Types from "./mutation-types"
 
 let idStart = 1
 export default {
-  [Types.ADDTODO](state,todo){
-    if(!todo.id) {
-      todo.id=++idStart
+  [Types.ADDTODO](state, todo) {
+    if (!todo.id) {
+      todo.id = ++idStart
     }
     state.todos.push(todo)
   },
 
-  [Types.DELTODO](state,todo){
-    state.todos.forEach((item,index) => {
-      if(item.id=== todo.id ) {
-        state.todos.splice(index,1)
+  [Types.DELTODO](state, todo) {
+    state.todos.forEach((item, index) => {
+      if (item.id === todo.id) {
+        state.todos.splice(index, 1)
       }
     })
   }
